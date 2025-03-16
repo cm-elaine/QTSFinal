9bcac82b285f8185699cd3bc9d5350f3e0362be5
cm-elaine <elainecalazans14@gmail.com>
2025-03-16 00:07:22 -0300

    issue 'post' is missing in props validation

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -1,26 +1,11 @@
-import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
-import styles from "./PostDetail.module.css";
-
-const PostDetail = ({ post }) => {
-  return (
-    <div className={styles.post_detail}>
-      <img src={post.image} alt={post.title} />
-      <h2>{post.title}</h2>
-      <p className={styles.createdby}>por: {post.createdBy}</p>
-      <div className={styles.tags}>
-        {post.tags.map((tag) => (
-          <p key={tag}>
-            <span>#</span>
-            {tag}
-          </p>
-        ))}
-      </div>
-      <Link to={`/posts/${post.id}`} className="btn btn-outline">
-        Ler
-      </Link>
-    </div>
-  );
+PostDetail.propTypes = {
+  post: PropTypes.shape({
+    image: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    createdBy: PropTypes.string.isRequired,
+    tags: PropTypes.arrayOf(PropTypes.string).isRequired,
+    id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+  }).isRequired,
 };
-
-export default PostDetail;
