6c42633e3d1f7fe627f49f9f149adb0020a83a9c
cm-elaine <elainecalazans14@gmail.com>
2025-03-17 16:54:12 -0300

    tratativa de cobertura de testes para hooks

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,18 +1,8 @@
-
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore"
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAVs5XvQIyn_Owg6E5HRwu4POL45F1NYXw",
-  authDomain: "miniblog-90a31.firebaseapp.com",
-  projectId: "miniblog-90a31",
-  storageBucket: "miniblog-90a31.appspot.com",
-  messagingSenderId: "53511476285",
-  appId: "1:53511476285:web:c05cc3aebb4fe9e2640654"
-};
+import { getFirestore } from "firebase/firestore";
+import { firebaseConfig } from "./firebaseConfig";
 
 const app = initializeApp(firebaseConfig);
-
 const db = getFirestore(app);
 
-export { db };
+export { db, getFirestore };
---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAVs5XvQIyn_Owg6E5HRwu4POL45F1NYXw",
+  authDomain: "miniblog-90a31.firebaseapp.com",
+  projectId: "miniblog-90a31",
+  storageBucket: "miniblog-90a31.appspot.com",
+  messagingSenderId: "53511476285",
+  appId: "1:53511476285:web:c05cc3aebb4fe9e2640654"
+};
+
+export default firebaseConfig;
